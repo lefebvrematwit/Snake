a19b9a66c7b8fb97197c30a52bce103c5f7a4b4f
Chan-Rodney <31736541+Chan-Rodney@users.noreply.github.com>
2019-11-28 17:25:18 -0500

    This is the score class that will be used in Score pane

---
+++ src/Score.java
@@ -0,0 +1,20 @@
+
+public class Score {
+	
+	private int score;
+	
+	public Score (int score)
+	{
+		this.score = score;
+	}
+	public int getScore()
+	{
+		return score;
+	}
+	public void getScore(int point)
+	{
+		score = point;
+	}
+	
+
+}
